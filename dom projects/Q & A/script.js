a9fc679717f73ed1e0bd02c52869a31ab62bb4a0
= <omsharmaitm@gmail.com>
2025-06-18 21:42:16 +0530

    small bug updated

--- dom projects/Q & A/script.js
+++ dom projects/Q & A/script.js
@@ -3,7 +3,7 @@ que.forEach(function(q){
     let btn = q.querySelector(".btns");
     btn.addEventListener("click" , function(){
         let answer = q.querySelector(".answer");
-        answer.classList.toggle("show");
+        answer.classList.toggle("hide");
         //button css update
         let i = btn.querySelector("i");
         i.classList.toggle("fa-plus-square");
