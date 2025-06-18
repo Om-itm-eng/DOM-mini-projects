796592317a0ac04ed02c9338d32d5d1dc0394b0c
= <omsharmaitm@gmail.com>
2025-06-18 21:31:42 +0530

    added Q&A project

---
+++ dom projects/Q & A/script.js
@@ -0,0 +1,13 @@
+const que = document.querySelectorAll(".Q");
+que.forEach(function(q){
+    let btn = q.querySelector(".btns");
+    btn.addEventListener("click" , function(){
+        let answer = q.querySelector(".answer");
+        answer.classList.toggle("show");
+        //button css update
+        let i = btn.querySelector("i");
+        i.classList.toggle("fa-plus-square");
+        i.classList.toggle("fa-minus-square");
+        
+    })
+});
