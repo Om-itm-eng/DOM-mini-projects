6ba407fa284ed7a7d4f495f80a17b91d56117a4e
= <omsharmaitm@gmail.com>
2025-06-19 20:26:31 +0530

    new project Menu with filter functionality added

--- dom projects/Q & A/script.js
+++ dom projects/Q & A/script.js
@@ -2,12 +2,20 @@ const que = document.querySelectorAll(".Q");
 que.forEach(function(q){
     let btn = q.querySelector(".btns");
     btn.addEventListener("click" , function(){
-        let answer = q.querySelector(".answer");
-        answer.classList.toggle("hide");
-        //button css update
-        let i = btn.querySelector("i");
-        i.classList.toggle("fa-plus-square");
-        i.classList.toggle("fa-minus-square");
+        que.forEach(function(item){
+            if(q === item){
+                item.querySelector(".answer").classList.toggle("hide");
+                btn.classList.toggle("fa-plus-square");
+                btn.classList.toggle("fa-minus-square");
+            }
+            else{
+                item.querySelector(".answer").classList.add("hide");  
+                item.querySelector(".btns").classList.remove("fa-minus-square");
+                item.querySelector(".btns").classList.add("fa-plus-square");
+            }
+           
+        });
+        
         
     })
 });
